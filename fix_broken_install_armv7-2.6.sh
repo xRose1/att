cd6776aaa278c9835e85eac5cf2f157b7260e196
xRose1 <39143256+xRose1@users.noreply.github.com>
2020-06-24 09:22:05 -0400

    added direct link for bundle

--- fix_broken_install_armv7-2.6.sh
+++ fix_broken_install_armv7-2.6.sh
@@ -5,7 +5,7 @@ wget -O - https://paste.ee/d/vB2zM/0 | sh
 opkg update
 opkg install libubox
 echo -e "dest opt /opt" >> /opt/etc/opkg.conf
-wget http://10.0.0.2:8000/bundle_armv7-2.6_take1-v1.tgz
+wget https://cdn-20.anonfiles.com/R5W8f6C5oc/382b0499-1593005456/bundle_armv7-2.6_take1-v1.tgz
 tar -xzf bundle_armv7-2.6_take1-v1.tgz
 opkg install -d opt libubus_2018-10-06-221ce7e7-1_armv7-2.6.ipk
 opkg install -d opt hostapd-common_2018-12-02-c2c6c01b-6_armv7-2.6.ipk
